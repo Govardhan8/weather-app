0926c5a19a41c0a3747075944c52b4b15284b5e4
Govardhan8 <govardhanmurthy007@gmail.com>
2023-08-21 18:55:28 +0530

    Inital commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "fce2d7cd014d86af6c0637f27f96ebff",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
