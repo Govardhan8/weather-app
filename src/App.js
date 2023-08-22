ffaa4966c0a50313be2a81ef7976525f32c23078
Govardhan8 <govardhanmurthy007@gmail.com>
2023-08-22 17:04:16 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
           Download Source Code
         </a>{" "}
         | Developed by{" "}
-        <a target="_blank" href="https://www.gauravghai.dev/">
-          Gaurav Ghai
+        <a target="_blank" href="https://govardhan-portfolio.netlify.app/">
+          Govardhan N
         </a>{" "}
         | Powered by{" "}
         <a target="_blank" href="https://www.htmlhints.com/">
